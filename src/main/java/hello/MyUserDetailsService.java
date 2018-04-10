d2b4a8b738bd9c4f8d04fdaf38e3a5244a6bbed2
janusarek <jan.usarek@gmail.com>
2018-04-10 14:56:45 +0200

    Add some users with bcyrpt encoded passwords

--- src/main/java/hello/MyUserDetailsService.java
+++ src/main/java/hello/MyUserDetailsService.java
@@ -17,6 +17,15 @@ public class MyUserDetailsService implements UserDetailsService {
         // we will find user details by some other means e.g. from an external system
         users.add(new UserObject("erin", "{noop}123", "ADMIN")); // It may not work without explicit encoder e.g. {noop}
         users.add(new UserObject("mike", "{noop}234", "ADMIN"));
+        users.add(new UserObject("todd", "{bcrypt}$2a$04$o8D4uVjGvYZjGo84peVz1.wVZatn80GWjRctOrUJoS5FH7RPNPjSi", "ADMIN")); // 'password2'
+        users.add(new UserObject("suzy", "{bcrypt}$2a$04$9/4eXcGI8re4JApkU.c.jOk330BfMWsqkvWmitC2WYTYUnWS9UHSu", "ADMIN")); // '123456'
+        users.add(new UserObject("matt", "{bcrypt}$2a$04$dUQbLsYq.3hgFp7jWQQj2eV0jR1KDOtvnFZA9hvRveChqv50d32TK", "ADMIN")); // '123456'
+        users.add(new UserObject("alice", "{bcrypt}$2a$04$N7lX6KegKqJSgOPtrEos3e/miHTOJ9t8kfFl0coW8KixfAhWSS14y", "ADMIN")); // '123456'
+
+        // List of possible password encoders:
+        // https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/crypto/password/DelegatingPasswordEncoder.html
+        // Usefull bcrypt calculator:
+        // https://www.dailycred.com/article/bcrypt-calculator
     }
 
     @Override
